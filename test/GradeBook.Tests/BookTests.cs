9e5732c2a4cd8c65bbb3f97365967f3b7598e85c
behistun <behistun@users.noreply.github.com>
2021-02-07 19:23:48 -0600

    Testing updated.

--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -11,16 +11,17 @@ namespace GradeBook.Tests
             // arrange section
             var book = new Book("");
             book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.3);
+            book.AddGrade(-5.1); //should not add and should not be the low value.
+            book.AddGrade(105.0); //should not add and should not be the high value.
+            book.AddGrade(88.9);
 
             // act section
             var result = book.GetStatistics();
 
             // assert section
-            Assert.Equal(85.6, result.Average, 1);
-            Assert.Equal(90.5, result.High, 1);
-            Assert.Equal(77.3, result.Low, 1);
+            Assert.Equal(89.0, result.Average, 1);
+            Assert.Equal(89.1, result.High, 1);
+            Assert.Equal(88.9, result.Low, 1);
         }
     }
 }
